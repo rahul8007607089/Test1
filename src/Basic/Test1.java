78ef6df3f75fcb0d6557e074eff31e1ff8a6fc33
pc <pc@DESKTOP-5S81P6M>
2022-10-10 16:16:16 +0530

    First commit

---
+++ src/Basic/Test1.java
@@ -0,0 +1,16 @@
+package Basic;
+
+public class Test1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("main method is running");
+		
+		m1();
+	}
+
+	public static void m1()
+	{
+		System.out.println("Hi");
+	}
+}
